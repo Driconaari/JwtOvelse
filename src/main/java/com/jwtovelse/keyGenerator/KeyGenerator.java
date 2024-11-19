78f131a6ab3102ab15165b91cb2b7dcc09c03b7b
asger <asgervb@gmail.com>
2024-11-19 19:22:05 +0100

    added a keygenerator the you can input into our Envoirmental varialbe

--- src/main/java/com/jwtovelse/keyGenerator/KeyGenerator.java
+++ src/main/java/com/jwtovelse/keyGenerator/KeyGenerator.java
@@ -3,6 +3,14 @@ package com.jwtovelse.keyGenerator;
 import java.security.SecureRandom;
 import java.util.Base64;
 
+//READ ME
+//This class is used to generate a secret key for the JwtUtil class
+//The secret key is used to sign the JWT token
+//The secret key should be kept secret and should not be shared with anyone
+//The secret key should be stored in a secure location
+//The secret key should be a random string of characters
+//USE THE KEY IN THE ENVIRONMENT VARIABLE IN PROJECTION SETTINGS
+
 public class KeyGenerator {
 
     public static String generateKey() {
