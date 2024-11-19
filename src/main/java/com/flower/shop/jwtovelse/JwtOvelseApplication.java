46e96c1b952eea813eb7bda28f2243bc069c453d
asger <asgervb@gmail.com>
2024-11-19 12:18:31 +0100

    Initial commit

---
+++ src/main/java/com/flower/shop/jwtovelse/JwtOvelseApplication.java
@@ -0,0 +1,13 @@
+package com.flower.shop.jwtovelse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtOvelseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtOvelseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flower/shop/jwtovelse/JwtOvelseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flower.shop.jwtovelse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtOvelseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
